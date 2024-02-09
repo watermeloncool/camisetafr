533e0ec78a854341cc7315ad5bc6cfb8a53bc561
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-02-09 16:27:26 +0100

    Add files via upload

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,3 +1,50 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed", headers: {"Allow": "POST"} };
+  }
+
+  const { price } = JSON.parse(event.body); // Récupérer le prix du corps de la requête
+
+  try {
+    // Créer une session de paiement Stripe
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'chf',
+          product_data: {
+            name: 'Custom Product',
+          },
+          unit_amount: price * 100, // Stripe utilise les plus petites unités de monnaie
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: 'https://splendid-macaron-38db40.netlify.app/success.html', // Remplacer par votre URL de succès
+      cancel_url: 'https://splendid-macaron-38db40.netlify.app/cancel.html', // Remplacer par votre URL d'annulation
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        // Spécifiez les headers CORS ici si nécessaire pour d'autres usages
+      },
+      body: JSON.stringify({ url: session.url }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
+
+
+
+
 /*
 
 // Importez la bibliothèque Netlify Functions
@@ -33,42 +80,3 @@ exports.handler = async (event, context) => {
 
 */
 
-const { handler } = require("@netlify/functions");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-
-exports.handler = async (event, context) => {
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
-
-  const { price } = JSON.parse(event.body);
-  // Valider le prix ici selon votre logique métier
-
-  try {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [{
-        // Utilisez un objet de prix ou définissez le prix directement
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'Custom Product',
-          },
-          unit_amount: price * 100, // Convertir en centimes
-        },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: `https://splendid-macaron-38db40.netlify.app/success.html`,
-      cancel_url: `https://splendid-macaron-38db40.netlify.app/cancel.html`,
-    });
-
-    return {
-      statusCode: 303,
-      headers: { Location: session.url },
-      body: ""
-    };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
-};
