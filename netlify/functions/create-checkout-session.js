bd94d10cf465b2c2b6dde1624b2e810dd0e94f47
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-02-09 16:39:50 +0100

    Add files via upload

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,6 +1,44 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed", headers: {"Allow": "POST"} };
+    }
+
+    const { price } = JSON.parse(event.body);
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+            price_data: {
+                currency: 'chf',
+                product_data: {
+                    name: 'Custom Product',
+                },
+                unit_amount: price * 100,
+            },
+            quantity: 1,
+        }],
+        mode: 'payment',
+        success_url: 'https://splendid-macaron-38db40.netlify.app/success.html',
+        cancel_url: 'https://splendid-macaron-38db40.netlify.app/cancel.html',
+    });
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url: session.url }),
+    };
+};
+
+
+
+/*
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed", headers: {"Allow": "POST"} };
   }
@@ -43,40 +81,4 @@ exports.handler = async (event) => {
 };
 
 
-
-
-/*
-
-// Importez la bibliothèque Netlify Functions
-const { handler } = require("@netlify/functions");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-// Fonction handler pour Netlify
-exports.handler = async (event, context) => {
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
-
-  try {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [{
-        price: 'price_1Ohp7bG4DSsnh5hVvBP27K1p',
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: `https://splendid-macaron-38db40.netlify.app/success.html`,
-      cancel_url: `https://splendid-macaron-38db40.netlify.app/cancel.html`,
-    });
-
-    return {
-      statusCode: 303,
-      headers: { Location: session.url },
-      body: ""
-    };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
-};
-
 */
-
